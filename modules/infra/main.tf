4d3676f9d713fdbaf7e3fc876e0f4f92df319c8c
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-20 23:18:36 +0530

    Update main.tf

--- modules/infra/main.tf
+++ modules/infra/main.tf
@@ -133,7 +133,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type   = var.ec2_instance_type
   subnet_id       = aws_subnet.public_a.id
   associate_public_ip_address = true
-  key_name        = var.ec2_key_pair
+
 
   tags = {
     Name        = "${var.project_name}-ec2-instance"
