f48a40fc9a3d610b1afe5440a7247e10ad8f0515
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-20 23:44:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = module.infra.vpc_id
+  value = module.aurora_db_setup.vpc_id
 }
 
 output "ec2_public_ip" {
-  value = module.infra.ec2_public_ip
+  value = module.aurora_db_setup.ec2_public_ip
 }
 
 output "aurora_endpoint" {
-  value = module.infra.aurora_endpoint
+  value = module.aurora_db_setup.aurora_cluster_endpoint
 }
