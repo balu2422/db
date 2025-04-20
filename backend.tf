745120f07364f27fa21e48265a9941360aee8350
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-20 23:16:43 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.1.0"
+  backend "s3" {
+    bucket         = "2-tier-architecture-modules"
+    key            = "tf/state4"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "2-tier-architecture-modules-table"
+  }
+}
