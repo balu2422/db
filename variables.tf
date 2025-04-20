8a9b3a9828f2fb69d44ee8146dcc635cab154d7d
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-20 23:40:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,65 +1,83 @@
 variable "project_name" {
   type        = string
-  default     = "my_project"
+  description = "A unique name for the project"
+  default     = "my-simple-aurora"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region to deploy to"
+  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
   type        = string
+  description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  description = "List of Availability Zones for subnets"
+  default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
 
-variable "ec2_instance_type" {
+variable "database_name" {
   type        = string
-  default     = "t2.micro"
-}
-
-variable "ec2_ami_id" {
-  type        = string
-  default     = "ami-0c55b159cbfafe1f0"
-}
-
-
-variable "aurora_cluster_name" {
-  type        = string
-  default     = "my-aurora-cluster"
+  description = "The initial database name"
+  default     = "mydatabase"
 }
 
 variable "db_engine" {
   type        = string
+  description = "The database engine type"
   default     = "aurora-mysql"
 }
 
 variable "db_engine_version" {
   type        = string
-  default     = "5.7"
+  description = "The database engine version"
+  default     = "8.0.mysql_aurora.3.05.2"
 }
 
-variable "database_name" {
+variable "db_instance_type" {
   type        = string
-  default     = "my_database"
+  description = "The instance type for Aurora nodes"
+  default     = "db.t4g.medium"
 }
 
 variable "db_master_username" {
   type        = string
+  description = "The master username for the Aurora cluster"
   default     = "admin"
+}
+
+variable "db_master_password_length" {
+  type        = number
+  description = "Length of the generated master password"
+  default     = 16
+}
+
+variable "aurora_cluster_name" {
+  type        = string
+  description = "The name of the Aurora cluster"
+  default     = "my-aurora-cluster"
+}
+
+variable "secrets_manager_secret_name" {
+  type        = string
+  description = "The name of the secret in AWS Secrets Manager"
+  default     = "aurora-master-credentials-v6"
 }
 
 variable "db_port" {
   type        = number
+  description = "The port the database listens on"
   default     = 3306
 }
 
 variable "allowed_inbound_cidrs" {
   type        = list(string)
-  default     = ["10.0.0.0/16"]
-}
-
-variable "db_instance_type" {
-  type        = string
-  default     = "db.r5.large"
+  description = "List of CIDR blocks allowed to access the AuroraDB"
+  default     = ["10.0.0.0/8"]
 }
