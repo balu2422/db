4aea87d0e7822e469ae61561be6a6936a8f1a8c3
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-20 23:53:05 +0530

    Update output.tf

--- modules/infra/output.tf
+++ modules/infra/output.tf
@@ -1,39 +1,17 @@
+
 output "vpc_id" {
   value       = aws_vpc.main.id
   description = "The ID of the VPC"
 }
 
 output "private_subnet_ids" {
-  value       = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id,
-    element(concat(aws_subnet.private_c.*.id, [""]), 0)
-  ]
+  value       = [aws_subnet.private_a.id, aws_subnet.private_b.id, element(concat(aws_subnet.private_c.*.id, [""]), 0)]
   description = "List of private subnet IDs"
-}
-
-output "public_subnet_ids" {
-  value       = [
-    aws_subnet.public_a.id,
-    aws_subnet.public_b.id,
-    element(concat(aws_subnet.public_c.*.id, [""]), 0)
-  ]
-  description = "List of public subnet IDs"
 }
 
 output "security_group_id" {
   value       = aws_security_group.aurora.id
   description = "The ID of the Aurora security group"
-}
-
-output "ec2_security_group_id" {
-  value       = aws_security_group.ec2.id
-  description = "The ID of the EC2 security group"
-}
-
-output "ec2_public_ip" {
-  value       = aws_instance.ec2.public_ip
-  description = "The public IP address of the EC2 instance"
 }
 
 output "aurora_cluster_endpoint" {
